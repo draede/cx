4aab173825395d839654baa17fdb119dc4ecac71
draede <draede@outlook.com>
2015-04-14 11:29:41 +0300

    0.1.2

--- Include/CX/C/Version.h
+++ Include/CX/C/Version.h
@@ -34,7 +34,7 @@
 
 #define CX_VER_MAJOR               0
 #define CX_VER_MINOR               1
-#define CX_VER_REVISION            3
+#define CX_VER_REVISION            2
 #define CX_VER_BUILD               0
 
 #define CX_VER_NAME                "CX" 
