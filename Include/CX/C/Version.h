8cc01cf054b405f8d31647056297f74e44e0b7ec
draede <draede@outlook.com>
2017-02-25 21:27:35 +0200

    0.2.109 - 2017-02-25 - Removed log & db files
    0.2.109 - 2017-02-25 - Removed log & db files
    - Removed log & db files

--- Include/CX/C/Version.h
+++ Include/CX/C/Version.h
@@ -34,7 +34,7 @@
 
 #define CX_VER_MAJOR               0
 #define CX_VER_MINOR               2
-#define CX_VER_BUILD               108
+#define CX_VER_BUILD               109
 #define CX_VER_REVISION            0
 
 #define CX_VER_NAME                "CX" 
