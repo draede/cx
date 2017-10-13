8cbcf9749bb0922fc12aa4fb609a8116c460f925
draede <draede@outlook.com>
2017-10-13 11:27:01 +0300

    - Fixed binary releases

--- create_bin_release.bat
+++ create_bin_release.bat
@@ -6,8 +6,7 @@ xcopy "%DIR%Bin\*cx.dll" "%DIR%Temp\BIN\Bin\" /sy
 xcopy "%DIR%Lib\*cx.pdb" "%DIR%Temp\BIN\Lib\" /sy
 xcopy "%DIR%Lib\*cx.lib" "%DIR%Temp\BIN\Lib\" /sy
 xcopy "%DIR%Include" "%DIR%Temp\BIN\Include\" /sy
-xcopy "%DIR%Contrib\*.h" "%DIR%Temp\BIN\Contrib\" /sy
-xcopy "%DIR%Contrib\*.hpp" "%DIR%Temp\BIN\Contrib\" /sy
+xcopy "%DIR%Contrib\*.*" "%DIR%Temp\BIN\Contrib\" /sy
 FOR /D /R "%DIR%Temp\BIN\Contrib" %%X IN (Src) DO RMDIR /S /Q "%%X" 2> NUL
 "%DIR%Tool\Bin\7za.exe" a -t7z -mx9 "%DIR%Temp\cx-bin.7z" "%DIR%Temp\BIN\*"
 rmdir /q /s "%DIR%Temp\BIN"
