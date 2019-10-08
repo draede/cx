665e4bf61463d2b657ac6ae1bb24a2bb58ff0931
draede <draede@outlook.com>
2019-10-08 21:39:51 +0300

    - Updated sqlite to version 3.30.0  - Updated xxHash to version 0.7.1  - Updated LZ4 to version 1.9.2  - Updated Expat to version 2.2.9  - Updated cURL to version 7.66.0

--- Include/CX/C/Version.h
+++ Include/CX/C/Version.h
@@ -34,7 +34,7 @@
 
 #define CX_VER_MAJOR               0
 #define CX_VER_MINOR               2
-#define CX_VER_BUILD               241
+#define CX_VER_BUILD               242
 #define CX_VER_REVISION            0
 
 #define CX_VER_NAME                "CX" 
